c9ff3459b1625bf798585fe114a7f0df53b32bdd
Dalibor Koenig <dalibor.koenig@gmail.com>
2021-08-18 14:55:27 +0200

    done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [show, setShow] = useState(false);
   return (
     <div className="App">
-      <button style={{color: show && "#D1D3C4", border: show && "2px solid #D1D3C4"}} onClick={() => setShow(!show)}>{show ? 'CLOSE' : 'MENU'}</button>
+      <button style={{color: show && "#DFE0DC", border: show && "2px solid #DFE0DC"}} onClick={() => setShow(!show)}>{show ? 'CLOSE' : 'MENU'}</button>
       <h1 style={{filter: show && "blur(5px)"}}>HELLO WORLD</h1>
       <div className="menu" style={{visibility: show && "visible", opacity: show && "1"}}>
         <p>HOME</p>
